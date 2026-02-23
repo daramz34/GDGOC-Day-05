8bf163f03554ca8e6470f6b6739b31f2db35dd3f
Darasimi Rotimi <darasimirotimi@gmail.com>
2026-02-23 14:48:40 +0100

    Trying to add this stuff

--- main.py
+++ main.py
@@ -12,3 +12,16 @@ def search(
     q: Annotated[str, Query(min_length=1)]
 ):
     return {"you_searched_for": q}
+
+
+@app.get("/products")
+def get_products(
+    q: Annotated[str, Query(min_length=1)],
+    category: str = "all",
+    max_price: float = 1000
+):
+    return{
+        "Search": q,
+        "Category": category,
+        "Max Price": max_price
+    }
